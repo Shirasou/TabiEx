216068e0780daccd7405ef709bd2a85fd07f389b
Shirasou <iamsoichiroshiramatsu@gmail.com>
2021-09-08 03:29:27 +0000

    [add] follow function

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
     resource :trip_likes, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
+  resources :users, only: [:index, :show, :edit, :update] do
+  	resource :relationships, only: [:create, :destroy]
+  	get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
