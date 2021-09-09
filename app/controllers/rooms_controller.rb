7f185930c418765d4d37fdb3ec4a1105c687878d
Shirasou <iamsoichiroshiramatsu@gmail.com>
2021-09-09 09:15:33 +0000

    [add] chat function

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,21 +1,27 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
-    @room = Room.create
+    @room = Room.create(user_id: current_user.id)
     @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
-    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
+    @entry2 = Entry.create(entry_params)
     redirect_to "/rooms/#{@room.id}"
   end
 
   def show
     @room = Room.find(params[:id])
     if Entry.where(user_id: current_user.id,room_id: @room.id).present?
-      @messages = @room.messages
+      @messages = @room.messages.includes(:user).order("created_at asc")
       @message = Message.new
       @entries = @room.entries
     else
       redirect_back(fallback_location: root_path)
     end
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id)
   end
 end
