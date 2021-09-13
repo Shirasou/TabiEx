427515ed91f28f03dcf75e54c5253ec7b461bbe7
Shirasou <iamsoichiroshiramatsu@gmail.com>
2021-09-13 15:25:21 +0000

    [add] category function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_categories
 
   private
 
@@ -13,5 +14,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :sex, :birth_at])
+  end
+
+  def set_categories
+    @categories = Category.all
   end
 end
