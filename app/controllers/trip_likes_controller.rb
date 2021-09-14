240b579c328938904e23073412fc88ec128002f1
Shirasou <iamsoichiroshiramatsu@gmail.com>
2021-09-14 13:47:52 +0000

    [add] asynchronous communication

--- app/controllers/trip_likes_controller.rb
+++ app/controllers/trip_likes_controller.rb
@@ -6,13 +6,11 @@ class TripLikesController < ApplicationController
     trip_like = @trip.trip_likes.new(user_id: current_user.id)
     trip_like.save
     @trip.create_notification_by(current_user)
-    redirect_to request.referrer
   end
 
   def destroy
     @trip = Trip.find(params[:trip_id])
     trip_like = @trip.trip_likes.find_by(user_id: current_user.id)
     trip_like.destroy
-    redirect_to request.referer
   end
 end
