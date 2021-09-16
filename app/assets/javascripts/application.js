ab847370322f4f69f9c56e351a45936f0b408b74
Shirasou <iamsoichiroshiramatsu@gmail.com>
2021-09-16 13:57:26 +0000

    save

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,33 @@
 //= require_tree .
 //= require cocoon
 
+$(function(){
+  /*=================================================
+  スマホメニュー
+  ===================================================*/
+  // ハンバーガーメニューのクリックイベント
+  // 解説は、「中級編：ストアサイト（インテリア）」参照
+  $('.toggle_btn').on('click', function() {
+    if ($('header').hasClass('open')) {
+      $('header').removeClass('open');
+    } else {
+      $('header').addClass('open');
+    }
+  });
+
+  // #maskのエリアをクリックした時にメニューを閉じる
+  $('#mask').on('click', function() {
+    $('header').removeClass('open');
+  });
+
+  // リンクをクリックした時にメニューを閉じる
+  $('nav a').on('click', function() {
+    $('header').removeClass('open');
+  });
+});
+
+
+
 $(document).on('turbolinks:load', function() {
 
   $('#tirp_images').on('change',function(e){
