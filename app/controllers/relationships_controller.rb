672982d5c3d94e096c2c5cf43a4845095acfbf71
Shirasou <iamsoichiroshiramatsu@gmail.com>
2021-09-26 07:23:27 +0000

    [modified] layout

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -14,12 +14,12 @@ class RelationshipsController < ApplicationController
 	end
 
   def followings
-		user = User.find(params[:user_id])
-		@users = user.followings
+		@user = User.find(params[:user_id])
+		@users = @user.followings
   end
 
   def followers
-		user = User.find(params[:user_id])
-		@users = user.followers
+		@user = User.find(params[:user_id])
+		@users = @user.followers
   end
 end
