bf543bca4c871bb2e2149fab152d51d70612536d
Soichiro <iamsoichiroshiramatsu@gmail.com>
2021-09-28 06:47:08 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'homes#top'
+  devise_for :users
   resources :trips do
     resource :trip_likes, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
