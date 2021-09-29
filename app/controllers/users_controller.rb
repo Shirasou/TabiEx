066e093ae2838e1a11f8489432536981906adaa4
Soichiro <iamsoichiroshiramatsu@gmail.com>
2021-09-29 08:49:23 +0000

    modified like function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,22 +5,6 @@ class UsersController < ApplicationController
   def index
     @users = User.page(params[:page]).per(6)
     @user = current_user
-    @user_entry1 = Entry.where(user_id: current_user.id)
-    @user_entry2 = Entry.where(user_id: @user.id)
-    unless @user.id == current_user.id
-      @user_entry1.each do |cu|
-        @user_entry2.each do |u|
-          if cu.room_id == u.room_id
-            @is_room = true
-            @room_id = cu.room_id
-          end
-        end
-      end
-      unless @is_room
-        @room = Room.new
-        @entry = Entry.new
-      end
-    end
   end
 
   def show
